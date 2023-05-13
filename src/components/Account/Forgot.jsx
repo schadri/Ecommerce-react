c9baa3d6126bd1374c39a2d8d5b32b8c43b8d446
schadri <adrianelromano@gmail.com>
2023-05-13 16:10:01 -0300

    Ultimos retoques

--- src/components/Account/Forgot.jsx
+++ src/components/Account/Forgot.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+// import React from 'react'
 
-export const Forgot = () => {
+// export const Forgot = () => {
+//   return (
+//     <div>
+//         <h1>EN DESARROLLO</h1>
+        
+//     </div>
+//   )
+// }
+
+import React, {useEffect } from 'react';
+import "./forgot.css"
+
+const RedirectComponent = () => {
+
+  useEffect(() => {
+    setTimeout(() => {
+      window.location.replace('/login')
+    }, 5000);
+
+  }, []);
+
   return (
     <div>
-        <h1>EN DESARROLLO</h1>
+      <h1 className='titulo text-center my-5'>Esto sigue en desarrollo</h1>
+      <h2 className='subtitulo text-center '>Redirección en 5 segundos...</h2>
     </div>
-  )
-}
+  );
+};
+
+export default RedirectComponent;
+
+
+
+
